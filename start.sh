859c7aeaf38ff1dfa7d05e3178b419957516cd28
wangchenglong-hj <wangchenglong-hj@huafang.com>
2023-05-12 19:43:04 +0800

    优化启动说明

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 chmod +x detectHead
 
-# 默认配置启动方案（如不使用默认方案，请在下行前边加上井号）
-./detectHead
-
-# 个性化配置启动方案（如需使用，请删除下行前边的井号）
-# ./detectHead -c config/custom.ini
+# 个性化配置启动方案
+./detectHead -c config/custom.ini
